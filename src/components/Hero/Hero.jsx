4ff3b5e1de490684b8af503ebe538956db0d707d
daddyloyal <lifeofemmany6@gmail.com>
2023-02-17 00:38:04 +0100

    Added the hero component, still not done tho

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,25 @@
+import Image from 'next/image';
+import HeroMusic from '../../assets/hero-music1.png';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="Hero my-28 px-20">
+        <div className="flex justify-between py-3">
+            <div className="text-section max-w-md">
+                <h1 className="text-xl md:text-3xl lg:text-6xl font-extrabold font-inter tracking-normal uppercase">A place for <span className="bg-black rounded-lg text-white leading-normal p-1">Classical</span> Music</h1>
+                <div className='bg-white rounded-full w-fit py-8 px-5 my-3'>
+                    <p className='text-base text-black font-inter font-semibold'>Ultra Lo-fi Dreams</p>
+                </div>
+            </div>
+            <div className="image-section">
+                <Image src={HeroMusic} alt="Hero Music" />
+            </div>
+            <div className="extra-info">
+                <p className='text-base font-semibold font-inter max-w-sm'>Compact and portable record cutter</p>
+            </div>
+        </div>
+    </section>
+  )
 };
 
 export default Hero;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,14 +5,14 @@ const Nav = () => {
     <nav className="w-full">
         <div className="py-5 flex justify-between items-center navbar mx-auto px-20">
             <div className="nav-link">
-                <ul>
+                <ul className="hidden md:block">
                     <li className="text-xl font-semibold">Home</li>
                 </ul>
             </div>
-            <div className="logo">
+            <div className="logo justify-center">
                 <h2 className="text-black font-inter font-black text-4xl">OPERATIC</h2>
             </div>
-            <button className="button outline outline-2 rounded-lg px-5 py-3 font-semibold font-inter" role="button">
+            <button className="button outline outline-2 rounded-lg px-5 py-3 font-semibold font-inter hidden md:block" role="button">
                 CONTACT US
             </button>
         </div>
