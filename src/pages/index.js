23ff7a2f123c529f2ca30f16fff4ba5ffd73a273
amarapeace <onwunzoamara@yahoo.com>
2023-02-17 06:41:50 +0100

    copy index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,11 @@
+import Hero from "@/components/Hero/Hero";
+import Nav from "@/components/Nav/Nav";
 
-
+export default function Home() {
+  return (
+    <div className="App bg-primary">
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
