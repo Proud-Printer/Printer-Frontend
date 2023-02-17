0d9e3fe6bafbe77644721a9c640d8d01440d701b
amarapeace <onwunzoamara@yahoo.com>
2023-02-17 06:26:29 +0100

    search bar with filter function

---
+++ src/Components/DJ.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DJComponent() {
+  return (
+    <>
+      <h1> Signup</h1>
+      <p>login</p>
+    </>
+  );
+}
---
+++ src/Components/spotify.js
@@ -0,0 +1,4 @@
+const authendpoint = "https://accounts.spotify.com/authorize"
+const clientID = "115c508d62a248648d20b7458631398a"
+const redirectUrl = "https://localhost:3000"
+const scopes = ""
