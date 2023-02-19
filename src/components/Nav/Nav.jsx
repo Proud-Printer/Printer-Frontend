726e199265fd9fe29d2bc66b0b5890e6a1fc19f8
olawanle temitayo <olawanletemitayo@gmail.com>
2023-02-19 20:58:54 +0100

    feat: added a logout button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,16 @@
+import { useRouter } from 'next/router';
+import Cookie from 'universal-cookie';
+
 const Nav = () => {
+  const cookies = new Cookie();
+  const router = useRouter();
+
+  const user = cookies.get('token');
+
+  const handleLogout = () => {
+    cookies.remove('token');
+    window.location.href = '/';
+  };
   return (
     <nav className="w-full shadow-sm">
       <div className="py-5 flex justify-between items-center navbar mx-auto px-20">
@@ -12,12 +24,26 @@ const Nav = () => {
             OPERATIC
           </h2>
         </div>
-        <button
-          className="button outline outline-2 rounded-lg px-5 py-3 font-semibold font-inter hidden md:block"
-          role="button"
-        >
-          CONTACT US
-        </button>
+        {user ? (
+          <div className="nav-link">
+            <ul className="hidden md:block">
+              <li
+                className="text-xl text-[#fff] font-semibold py-2 px-4 bg-red-500 hover:bg-red-700 cursor-pointer rounded-lg
+              "
+                onClick={handleLogout}
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <button
+            className="button  rounded-lg px-5 py-3 font-semibold font-inter hidden md:block"
+            role="button"
+          >
+            CONTACT US
+          </button>
+        )}
       </div>
     </nav>
   );
